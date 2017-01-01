73ff778250c227766ff224a649b359a4535fa769
stawiars <danielstawiarski@wp.pl>
2017-01-01 18:31:27 +0100

    Firsts exercises form session and Java Beans in Java EE.

--- 01_Servlets/src/pl/polonialiberum/servlet/TestServlet.java
+++ 01_Servlets/src/pl/polonialiberum/servlet/TestServlet.java
@@ -2,19 +2,17 @@ package pl.polonialiberum.servlet;
 
 import java.io.IOException;
 
-
 import javax.servlet.ServletException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
- // Servlet implementation class TestServlet
-
+/**
+ * Implementation of test servlet
+ */
 
 public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -22,14 +20,13 @@ public class TestServlet extends HttpServlet {
 	public TestServlet() {
 		super();
 	}
-      
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("¯¹danie GET przetworzono pomyœlnie");
 	}
 
